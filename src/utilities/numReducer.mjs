c184774604af5c977f46ac2c8bf7723b04099bff
Krystal304 <krystalwilliams864@gmail.com>
2024-11-07 13:40:40 -0500

    initial

---
+++ src/utilities/numReducer.mjs
@@ -0,0 +1,23 @@
+
+import numReducerAction from '../utilities/numReducerAction.mjs'
+
+
+function reducer(state, action){
+    switch(action.type){
+      case ACTION.INCREMENT: 
+        return {
+          count: state.count + 1}
+      case ACTION.DECREMENT:
+        return {
+          count: state.count - 1}
+      case ACTION.ADDBYVALUE:
+        return {
+          count: state.count + Number(action.payload)};
+      case ACTION.SUBBYVALUE:
+        return {
+          count: state.count - action.payload};
+          default:
+            return state;
+    }
+  }
+  export default reducer
---
+++ src/utilities/numReducerAction.mjs
@@ -0,0 +1,9 @@
+
+const ACTION = {
+    INCREMENT: 'increment',
+    DECREMENT: 'decrement',
+    ADDBYVALUE: 'addByValue',
+    SUBBYVALUE: 'subByValue',
+  };
+  
+  export default ACTION;
